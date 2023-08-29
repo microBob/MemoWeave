5cbe41e6db01f4bdea350f36d0b955fd43b8630b
Kenneth Yang <microbob@is88.com>
2023-08-29 10:06:50 -0700

    Detect key press and advance focus

--- lib/views/thread_view.dart
+++ lib/views/thread_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:isar/isar.dart';
@@ -62,7 +63,12 @@ class ThreadView extends HookConsumerWidget {
         ),
         Focus(
           onKeyEvent: (node, event) {
-            return KeyEventResult.handled;
+            if (event is KeyDownEvent &&
+                event.logicalKey == LogicalKeyboardKey.enter) {
+              print(node.nextFocus());
+              return KeyEventResult.handled;
+            }
+            return KeyEventResult.ignored;
           },
           child: Column(
             children: threadState.threadCollection.blocks
